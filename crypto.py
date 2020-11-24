4d697d4638fcfde600aaa60dddf2325c3bb03735
kennywilliams07 <kwilliamsprogramming@gmail.com>
2020-11-24 15:56:18 -0500

    print top 5 currencies to terminal

--- crypto.py
+++ crypto.py
@@ -13,3 +13,23 @@ headers = {
     'Accepts': 'application/json',
     'X-CMC_PRO_API_KEY': 'b49cfe6e-6793-4201-aa44-9cdff4767d71',
 }
+
+session = Session()
+session.headers.update(headers)
+
+try:
+    response = session.get(url, params=parameters)
+    data = json.loads(response.text)
+    print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+    print(e)
+
+session = Session()
+session.headers.update(headers)
+
+try:
+    response = session.get(url, params=parameters)
+    data = json.loads(response.text)
+    print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+    print(e)
